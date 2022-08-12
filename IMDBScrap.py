58708732d6c4e9565bc1d2699e4572a506f08c2a
Yunus Emre Sayın <87127328+YunusEmreSayin@users.noreply.github.com>
2022-08-12 15:42:40 +0300

    Add files via upload

---
+++ IMDBScrap.py
@@ -0,0 +1,38 @@
+import openpyxl
+import requests
+from bs4 import BeautifulSoup
+
+url="https://www.imdb.com/chart/top/" #Url we will connect
+r=requests.get(url)
+### Connecting url with get method
+
+soup=BeautifulSoup(r.content,"html5lib")
+### Creating soup object for getting html elements
+List=soup.find("tbody",{"class":"lister-list"}).find_all("tr")
+
+wb=openpyxl.Workbook()
+###Created workbook object
+sheet=wb.active
+###Created WorkSheet
+
+Row=1
+###Defined row variable for our loop
+for film in List:
+     Name=film.find("td",{"class":"titleColumn"}).a.text
+     Year=film.find("td",{"class":"titleColumn"}).span.text
+     Rating=film.find("td",{"class":"ratingColumn imdbRating"}).text.strip()
+     ###Reached the html element of movie data
+     FirstC=sheet.cell(column=1,row=Row)
+     SecondC=sheet.cell(column=2,row=Row)
+     ThirdC=sheet.cell(column=3,row=Row)
+     ###Defined columns
+     FirstC.value=Name
+     SecondC.value=Year
+     ThirdC.value=Rating
+     ###Assigned values to columns
+     Row+=1
+
+wb.save("ImdbTop250.xlsx")
+###Saved & Created xlsx file
+print("File saved successfully")
+
